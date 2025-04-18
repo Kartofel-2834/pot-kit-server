b14a752526a013775aa7d446f9ef980d7c91f830
Раджаб  Габибов <r.gabibov@goodt.me>
2025-04-18 20:34:26 +0300

    feat(index): inited pot-kit static server

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,16 @@
-import { Elysia } from "elysia";
+import { Elysia } from 'elysia';
+import { staticPlugin } from '@elysiajs/static';
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia();
 
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+// Styles static
+app.use(
+    staticPlugin({
+        assets: './src/data/styles-json',
+        prefix: '/styles',
+    }),
 );
+
+app.listen(3000);
+
+console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`);
---
+++ src/map-styles.ts
@@ -0,0 +1,86 @@
+import fs from 'node:fs/promises';
+import path, { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+function cutData(data: string, dataKey: string): Record<string, string> {
+    const result: Record<string, string> = {};
+
+    let condition = '';
+    let key = '';
+    let endMarker = '';
+
+    const startRegex = new RegExp(`/\\* ${dataKey}:[a-zA-Z]+ - START \\*/`);
+
+    for (let char of data) {
+        condition += char;
+
+        if (startRegex.test(condition)) {
+            const startIndex = condition.indexOf(`${dataKey}:`);
+            const endIndex = condition.indexOf(' - START');
+
+            key = condition.slice(startIndex + dataKey.length + 1, endIndex);
+            endMarker = `\/\* ${dataKey}:${key} - END \*\/`;
+            condition = '';
+        }
+
+        if (endMarker && condition.includes(endMarker)) {
+            condition = condition;
+            result[key] = condition.replace(endMarker, '');
+
+            condition = '';
+            key = '';
+            endMarker = '';
+        }
+    }
+
+    return result;
+}
+
+async function mapStylesToMap(dataKey: string, inputPath: string, outputPath: string) {
+    try {
+        const [dir] = await Promise.all([
+            fs.readdir(inputPath),
+            fs.mkdir(outputPath, { recursive: true }),
+        ]);
+
+        await Promise.all([
+            dir.map(async fileName => {
+                const componentName = path.basename(fileName, '.css');
+                const data = await fs.readFile(path.join(inputPath, fileName), 'utf8');
+                const stringifiedData = JSON.stringify(cutData(data, dataKey));
+
+                await fs.writeFile(path.join(outputPath, `${componentName}.json`), stringifiedData);
+            }),
+        ]);
+    } catch (err) {
+        console.warn(`[POT-STYLES-COMBINER]:mapStylesToMap`, err);
+    }
+}
+
+async function mapConditions() {
+    const inputPath = path.join(__dirname, 'data', 'styles', 'conditions');
+    const outputPath = path.join(__dirname, 'data', 'styles-json', 'conditions');
+
+    return mapStylesToMap('CONDITION', inputPath, outputPath);
+}
+
+async function mapConfigurations() {
+    const inputPath = path.join(__dirname, 'data', 'styles', 'configuration');
+    const outputPath = path.join(__dirname, 'data', 'styles-json', 'configuration');
+
+    return mapStylesToMap('CONFIGURATION', inputPath, outputPath);
+}
+
+async function init() {
+    try {
+        await Promise.all([mapConditions(), mapConfigurations()]);
+        console.log('[POT-STYLES-COMBINER]:init styles combined');
+    } catch (err) {
+        console.log('[POT-STYLES-COMBINER]:init error', err);
+    }
+}
+
+init();
